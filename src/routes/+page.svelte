52442135069547c9dffca82070f815c4fb14d3ae
Creedmastr <thegaminglife09@gmail.com>
2023-07-04 17:02:11 +0200

    functional, doing styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,128 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let goals: Array<String> = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ];
+
+    let possibleGoals = [
+        "5 Koroks",
+        "5 hearts",
+        "5 shrine",
+        "Gerudo's main quest",
+        "Zora's main quest",
+        "Goron's main quest",
+        "Rito's main quest",
+        "An abyss",
+        "A piece of Yiga armor",
+        "Go to the Lost Woods",
+        "Go to Kakariko village",
+        "Go to Atheno village",
+        "Go to a mine",
+        "Champion's tunic",
+    ];
+
+    let i = 0;
+    while (i < goals.length) {
+        let index = Math.floor(Math.random() * possibleGoals.length);
+        console.log(i);
+        goals[i] = possibleGoals[index];
+
+        i++;
+    }
+
+    let buttonColor: Array<string> = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]; // Initial button color
+
+    let timer: any;
+
+    function handleLongPress(buttonId: any) {
+        timer = setTimeout(() => {
+            buttonColor[buttonId] = "red";
+        }, 500); // Adjust the duration of long-press as needed
+    }
+
+    function handlePressEnd() {
+        clearTimeout(timer);
+    }
+
+    function handleClick(buttonId: any) {
+        console.log("Button ID:", buttonId);
+        buttonColor[buttonId] = "blue";
+    }
+</script>
+
+<body>
+    <div id="grid">
+        {#each goals as goal, index}
+            <button
+                on:mousedown|preventDefault={() => handleLongPress(index)}
+                on:mouseup|preventDefault={handlePressEnd}
+                on:mouseleave|preventDefault={handlePressEnd}
+                on:click|preventDefault={() => handleClick(index)}
+                style="background-color: {buttonColor[index]}"
+                id={`btn${index}`}
+            >
+                {goal}
+            </button>
+        {/each}
+    </div>
+</body>
+
+<style>
+    button {
+        display: subgrid;
+        font-size: 2rem;
+        background-color: lightgray;
+        filter: drop-shadow(0 0 0.15rem black);
+        border-radius: .1rem;
+        border: 0;
+    }
+</style>
